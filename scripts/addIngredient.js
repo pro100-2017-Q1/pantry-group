e34bcccbd2d5f5142140bc48b54973c1b25b1246
dkuhrmeyer <dkuhrmeyer@gmail.com>
2017-02-22 18:23:02 -0700

    removed ingredient label for when a user presses the add another button

--- scripts/addIngredient.js
+++ scripts/addIngredient.js
@@ -1,4 +1,4 @@
 function addIngredientFields(){
     var field = document.getElementById("addedIngredient");
-    field.innerHTML += "<br/><form class='form-horizontal'><div class='form-group'><label class='control-label col-sm-2' for='ingredientName'>Ingredients:</label><form method='POST'><div class='col-sm-4'><input type='text' class='form-control' name='myInputs[]' id='ingredientName' placeholder='Enter ingredient name'></div><div class='col-sm-2'><input type='number' class='form-control' id='ingredientNumber' placeholder='How many'></div></form>";
+    field.innerHTML += "<br/><form class='form-horizontal'><div class='form-group'><label class='control-label col-sm-2' for='ingredientName'></label><form method='POST'><div class='col-sm-4'><input type='text' class='form-control' name='myInputs[]' id='ingredientName' placeholder='Enter ingredient name'></div><div class='col-sm-2'><input type='number' class='form-control' id='ingredientNumber' placeholder='How many'></div></form>";
 }
