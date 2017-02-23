a9a716e022b6b483f7ea3eee554dc407a0aeaa9a
Daniel Carapia <toonlink4943@gmail.com>
2017-02-22 18:18:24 -0700

    Added test in tests.js to check if addInput changes the html when called.

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,3 +1,6 @@
-QUnit.test("hello test", function(assert){
-    assert.ok(1 == "1", "Passed");
+QUnit.test("Add Ingredient Test", function(assert){
+    var oldHtml = document.getElementById('qunit-fixture').innerHTML;
+    addInput('qunit-fixture')
+    var newHtml = document.getElementById('qunit-fixture').innerHTML;
+    assert.notEqual(oldHtml, newHtml);
 });
