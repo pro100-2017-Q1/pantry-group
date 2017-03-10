f712804e596b11aa2d50be719ee7e5fcc012667d
dkuhrmeyer <dkuhrmeyer@gmail.com>
2017-03-09 18:22:58 -0700

    added styling to the recipe page that is dynamic

--- script.js
+++ script.js
@@ -166,37 +166,43 @@ function displayRecipes(){
         var recipe = recipesList.recipes[index];
 
         recipeDiv = document.createElement("div");
+        recipeDiv.setAttribute("class", "recipeDiv");
         recipeDiv.id = recipe.name + "div";
 
         recipeNameDiv = document.createElement("div");
         recipeNameDiv.id = recipe.name;
-        recipeNameDiv.innerHTML = recipe.name;
+        recipeNameDiv.setAttribute("class", "recipeName");
+        recipeNameDiv.innerHTML = recipe.name + "<br />";
 
         recipeRequirements = document.createElement("ul");
         recipeRequirements.id = recipe.name + "Requirements";
+        recipeRequirements.setAttribute("class", "recipeIngredient");
         for (var index = 0; index < recipe.requiredIngredients.length; index++) {
             var item = recipe.requiredIngredients[index];
             requirementDiv = document.createElement("li");
             requirementDiv.id = item.name + "div";
-            requirementDiv.innerHTML = item.name + ": " + findIngredient(item.name).count + " / " + item.count;
+            requirementDiv.innerHTML = "Ingredients: " + "<br />" + item.name + ": " + findIngredient(item.name).count + " / " + item.count + "<br />";
             recipeRequirements.appendChild(requirementDiv);
         }
 
         recipeInstructionsDiv = document.createElement("div");
         recipeInstructionsDiv.id = recipe.name + "Instructions";
-        recipeInstructionsDiv.innerHTML = recipe.instructions;
+        recipeInstructionsDiv.setAttribute("class", "recipeInstructions");
+        recipeInstructionsDiv.innerHTML = "Instructions: " + "<br />" + recipe.instructions + "<br />";
 
         recipeCalorieDiv = document.createElement("div");
         recipeCalorieDiv.id = recipe.name + "Calories";
-        recipeCalorieDiv.innerHTML = recipe.totalCalories;
+        recipeCalorieDiv.setAttribute("class", "recipeCalorie");
+        recipeCalorieDiv.innerHTML = "Calories: " + "<br />" + recipe.totalCalories + "<br />";
 
 
         
         document.getElementById("ListOfRecipes").appendChild(recipeDiv);
         recipeDiv.appendChild(recipeNameDiv);
+        recipeDiv.appendChild(recipeInstructionsDiv);
         recipeDiv.appendChild(recipeCalorieDiv);
         recipeDiv.appendChild(recipeRequirements);
-        recipeDiv.appendChild(recipeInstructionsDiv);
+        
 
     }
     
